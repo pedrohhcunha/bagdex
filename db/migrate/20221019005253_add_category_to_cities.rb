2a4a23538d065c733b75ba67b5ab49b3d0959458
pedrohhcunha <pedro_h_cunha@estudante.sc.senai.br>
2022-10-21 19:02:06 -0300

    remove errors

--- db/migrate/20221019005253_add_category_to_cities.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToCities < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :cities, :category, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_225803) do
+ActiveRecord::Schema.define(version: 2022_10_19_005005) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
